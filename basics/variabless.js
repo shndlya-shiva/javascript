319eb0338bc71c3b8267b295320afe4d73ab6ed3
shndlya-shiva <shandilyashivansh153@gmail.com>
2026-08-30 23:46:27 +0530

    add new file

---
+++ basics/variabless.js
@@ -0,0 +1,12 @@
+const AcconutId = 378120371
+let AccountEmail = "shandilyashivansh0@gmail.com"
+var AccountPassword = "fdjvjdfbkj"
+
+console.log(AcconutId);
+console.table([AcconutId, AccountEmail, AccountPassword]) 
+
+/*
+const is used for variables that are constant
+let is used of variables that can be reassigned later
+var is the traditional keyword used to declare a variable but its preferd not be used bcz of its issue in functional and block scope
+*/
