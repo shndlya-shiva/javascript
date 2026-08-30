35da253a4e48821934741daf734d48e13bca4d9a
shndlya-shiva <shandilyashivansh153@gmail.com>
2026-08-30 23:49:28 +0530

    add new file

--- basics/variabless.js
+++ basics/variabless.js
@@ -1,12 +1,14 @@
 const AcconutId = 378120371
 let AccountEmail = "shandilyashivansh0@gmail.com"
 var AccountPassword = "fdjvjdfbkj"
+let AccountOwner;
 
 console.log(AcconutId);
-console.table([AcconutId, AccountEmail, AccountPassword]) 
+console.table([AcconutId, AccountEmail, AccountPassword, AccountOwner]) 
 
 /*
 const is used for variables that are constant
 let is used of variables that can be reassigned later
 var is the traditional keyword used to declare a variable but its preferd not be used bcz of its issue in functional and block scope
+declared variables with any value returns undefined output
 */
