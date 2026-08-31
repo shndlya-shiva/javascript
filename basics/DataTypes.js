3ed095dcd9862a4e8f3265059ce201bace19790d
shndlya-shiva <shandilyashivansh153@gmail.com>
2026-08-31 23:09:32 +0530

    add new file

--- basics/DataTypes.js
+++ basics/DataTypes.js
@@ -4,3 +4,15 @@
 
 console.log(3+3); 
 console.log("Matt")
+
+/* numbers -> 2 to power 53
+bigint 
+string -> ""
+boolean -> true/false  
+null -> standalone value
+undefined ->
+symbol -> unique
+object
+ */
+
+console.log(typeof "Murdock") // helps with understanding the type of variable
