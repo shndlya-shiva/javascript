0f3c0adc5dad8900be62136217409b3f40ed2b7b
shndlya-shiva <shandilyashivansh153@gmail.com>
2026-09-02 22:19:12 +0530

    add new file

--- basics/ConversionOperation.js
+++ basics/ConversionOperation.js
@@ -1,7 +1,48 @@
-let score = "33";
-
+let score = "33abc";
+let score1 = "33";
+let score2 = null;
+let score3 = undefined;
+let score4 = true;
+let score5 = "Matt Murdock";
 console.log(typeof score);
 console.log(typeof(score));
-
+console.log(typeof score1);
+console.log(typeof score2);
+console.log(typeof score3);
+console.log(typeof score4);
+console.log(typeof score5);
 let valueInNumber = Number(score);
+let valueInNumber1 = Number(score1);
+let valueInNumber2 = Number(score2);
+let valueInNumber3 = Number(score3);
+let valueInNumber4 = Number(score4);
+let valueInNumber5 = Number(score5);
 console.log(typeof valueInNumber);
+console.log(valueInNumber);
+console.log(typeof valueInNumber1);
+console.log(valueInNumber1);
+console.log(typeof valueInNumber2);
+console.log(valueInNumber2);
+console.log(typeof valueInNumber3);
+console.log(valueInNumber3);
+console.log(typeof valueInNumber4);
+console.log(valueInNumber4);
+console.log(typeof valueInNumber5);
+console.log(valueInNumber5);
+let isLoggedIn = 1
+let isLoggedIn1 = "Wilson Fisk";
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn);
+let booleanIsLoggedIn1 = Boolean(isLoggedIn1);
+console.log(booleanIsLoggedIn1);
+ 
+/* 
+1-> true
+0-> false
+"" -> false 
+*/
+
+let SomeNumber = 33;
+let SomeString = String (SomeNumber);
+console.log(typeof SomeString);
+console.log(SomeString);
