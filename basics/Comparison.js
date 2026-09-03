32b3eadc2e4d20df29235d9b130dc226da4391cc
shndlya-shiva <shandilyashivansh153@gmail.com>
2026-09-03 22:08:19 +0530

    add new file

---
+++ basics/Comparison.js
@@ -0,0 +1,14 @@
+console.log("********** COMPARISON **********");
+console.log(2 > 3);
+console.log(2 < 3);
+console.log(2 == 3);
+console.log(2 != 3);
+console.log(2 >= 3);
+console.log(2 <= 3);
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+console.log(undefined > 0);
+console.log(undefined == 0);
+console.log(undefined >= 0);
+console.log("2">2);
