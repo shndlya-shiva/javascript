b86eb9867f4fb7442eaadb769ad86c9fe669b1de
shndlya-shiva <shandilyashivansh153@gmail.com>
2026-09-05 23:48:04 +0530

    add new file

---
+++ basics/Strings.js
@@ -0,0 +1,6 @@
+const name = "John Doe";
+const age = 30;
+console.log(`My name is ${name} and I am ${age} years old.`); 
+// $ { } is used for string interpolation in JavaScript, allowing you to embed expressions within template literals.
+// ` ` represents template literals in JavaScript. This eliminates the need for messy plus-sign (+) concatenation.
+const gameName = new String("Chess");
