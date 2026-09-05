9b7e0cbb8daa479f6cfa803477b667bab2147857
shndlya-shiva <shandilyashivansh153@gmail.com>
2026-09-05 23:23:05 +0530

    add new file

--- basics/MemoryAllocation.js
+++ basics/MemoryAllocation.js
@@ -16,3 +16,10 @@ console.log("After changing anothername:");
 console.log(anothername); // Output: Doe
 console.log("Original name variable:");
 console.log(name); // Output: John (Original value remains unchanged)
+let user = { name: "Alice", age: 25 }; // Heap Memory
+let anotherUser = user; // Heap Memory (Reference to the same object)
+console.log("Before changing user.name:");
+console.log(anotherUser);
+user.name = "Bob"; // Modifying the object in Heap Memory
+console.log("After changing user.name:");
+console.log(anotherUser); // Output: { name: "Bob", age: 25 } (Reflects the change)
