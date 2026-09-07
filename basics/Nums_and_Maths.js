95f7afab1d25f3c5404ae7344604a4f05d391612
shndlya-shiva <shandilyashivansh153@gmail.com>
2026-09-07 21:58:25 +0530

    new file

--- basics/Nums_and_Maths.js
+++ basics/Nums_and_Maths.js
@@ -3,3 +3,8 @@ const balance = new Number(100);
 console.log(balance); // Output: [Number: 100]
 console.log(score); // Output: 100
 console.log(balance.toString()); // Output: "100"
+console.log(balance.toString()); // Output: "100"
+console.log(balance.toString().length); // Output: 3
+console.log(balance.toFixed(3)); // Output: "100.000"
+const num = 147.823456789;
+console.log(num.toPrecision(4)); // Output: "147.8"
