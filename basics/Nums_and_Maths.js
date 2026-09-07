a5bbf0bdc13c821255ebd1f701a345066d106c7d
shndlya-shiva <shandilyashivansh153@gmail.com>
2026-09-07 22:24:00 +0530

    updated file

--- basics/Nums_and_Maths.js
+++ basics/Nums_and_Maths.js
@@ -8,3 +8,14 @@ console.log(balance.toString().length); // Output: 3
 console.log(balance.toFixed(3)); // Output: "100.000"
 const num = 147.823456789;
 console.log(num.toPrecision(4)); // Output: "147.8"
+const num2 = 100000;
+console.log(num2.toLocaleString('en-IN')); // Output: "1,00,000"
+// ++++++++++++++ MATHS ++++++++++++++++
+console.log("++++++++++++++ MATHS ++++++++++++++++");
+console.log(Math.abs(-5)); // Output: 5
+console.log(Math.round(4.2)); // Output: 4
+console.log(Math.round(4.6)); // Output: 5
+console.log(Math.ceil(5.2)); // Output: 6
+console.log(Math.floor(5.8)); // Output: 5
+console.log(Math.random()); // Output: A random number between 0 (inclusive) and 1 (exclusive)
+console.log(Math.random()*100); // Output: A random number between 0 (inclusive) and 100 (exclusive)
