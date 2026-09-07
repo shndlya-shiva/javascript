18b8329c418172e735ade505763eff02a0f24eb7
shndlya-shiva <shandilyashivansh153@gmail.com>
2026-09-07 21:52:34 +0530

    new file

---
+++ basics/Nums_and_Maths.js
@@ -0,0 +1,5 @@
+const score = 100;
+const balance = new Number(100);
+console.log(balance); // Output: [Number: 100]
+console.log(score); // Output: 100
+console.log(balance.toString()); // Output: "100"
