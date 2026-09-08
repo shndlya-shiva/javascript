991f49e4aa993aea84b3221c97f0d3516e2d86de
shndlya-shiva <shandilyashivansh153@gmail.com>
2026-09-08 22:33:00 +0530

    add new file

---
+++ basics/Dates.js
@@ -0,0 +1,8 @@
+let myDate = new Date();
+console.log(myDate);
+console.log(myDate.toString());
+console.log(myDate.toDateString());
+console.log(myDate.toLocaleString());
+console.log(typeof myDate);
+let myDate2 = new Date(2023, 5, 15, 10, 30, 0);
+console.log(myDate2.toDateString());
