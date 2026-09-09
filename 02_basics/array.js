e269c67bfc19555c52f7126f05fe4ce6cc11674f
shndlya-shiva <shandilyashivansh153@gmail.com>
2026-09-10 00:04:45 +0530

    add new file

---
+++ 02_basics/array.js
@@ -0,0 +1,9 @@
+const myArr = [1, 2, 3, 4, 5];
+console.log(myArr);
+// Array Methods
+console.log(myArr.length);
+console.log(myArr[0]);
+myArr.unshift(11);
+console.log(myArr);
+myArr.shift();
+console.log(myArr);
