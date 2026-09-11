40ec55bc8980837e631f9a9299b0db21582e6cf7
shndlya-shiva <shandilyashivansh153@gmail.com>
2026-09-11 23:08:35 +0530

    updated file

--- 02_basics/Array02.js
+++ 02_basics/Array02.js
@@ -9,3 +9,10 @@ const combinedArray = arr1.concat(arr2);
 console.log(combinedArray); // Output: ["thor", "ironman", "hulk", "captain america", "black widow", "captain", "hawkeye", "antman", "spiderman", "doctor strange"]
 const combinedArray2 = [...arr1, ...arr2, ...arr3];
 console.log(combinedArray2); // Output: ["thor", "ironman", "hulk", "captain america", "black widow", "captain", "hawkeye", "antman", "spiderman", "doctor strange", "wanda", "vision", "sam", "bucky", "natasha"]
+const NumArray = [1, 2, 3, 4, 5,[6,7,8],9,10,[11,12,[13,14]]];
+console.log(NumArray); // Output: [1, 2, 3, 4, 5, [6, 7, 8], 9, 10, [11, 12, [13, 14]]]
+const flatArray = NumArray.flat(Infinity);
+console.log(flatArray); // Output: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]
+console.log(Array.isArray(arr1)); // Output: true
+console.log(Array.isArray(NumArray)); // Output: true
+console.log(Array.isArray("Shiva")); // Output: true
