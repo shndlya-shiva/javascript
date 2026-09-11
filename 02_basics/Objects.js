7f20f7fb8683a252a6b20f11f78431983dd23aea
shndlya-shiva <shandilyashivansh153@gmail.com>
2026-09-11 23:34:03 +0530

    add new file

---
+++ 02_basics/Objects.js
@@ -0,0 +1,8 @@
+// singleton object
+// object literals
+// Object.create() // creates a new object with the specified prototype object and properties.
+const myObj = {
+    name: "Shiva",
+    age: 20,
+    
+}
