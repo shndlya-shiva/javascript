86ebe6e632f0ff5399422c204296659714ae12a3
shndlya-shiva <shandilyashivansh153@gmail.com>
2026-09-11 22:27:59 +0530

    updated file

--- 02_basics/Array02.js
+++ 02_basics/Array02.js
@@ -1,9 +1,11 @@
-const arr1 = [1, 2, 3, 4, 5];
-const arr2 = [6, 7, 8, 9, 10];
-const arr3 = [11, 12, 13, 14, 15];
+const arr1 = ["thor", "ironman", "hulk", "captain america", "black widow"];
+const arr2 = ["captain", "hawkeye", "antman", "spiderman", "doctor strange"];
+const arr3 = ["wanda", "vision", "sam", "bucky", "natasha"];
 // Pushing array to an array
 arr1.push(arr2);
-console.log(arr1); // Output: [1, 2, 3, 4, 5, [6, 7, 8, 9, 10]]
+console.log(arr1); // Output: ["thor", "ironman", "hulk", "captain america", "black widow", ["captain", "hawkeye", "antman", "spiderman", "doctor strange"]]
 // Concatenating two arrays
-const combinedArray = arr2.concat(arr3);
-console.log(combinedArray); // Output: [6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+const combinedArray = arr1.concat(arr2);
+console.log(combinedArray); // Output: ["thor", "ironman", "hulk", "captain america", "black widow", "captain", "hawkeye", "antman", "spiderman", "doctor strange"]
+const combinedArray2 = [...arr1, ...arr2, ...arr3];
+console.log(combinedArray2); // Output: ["thor", "ironman", "hulk", "captain america", "black widow", "captain", "hawkeye", "antman", "spiderman", "doctor strange", "wanda", "vision", "sam", "bucky", "natasha"]
