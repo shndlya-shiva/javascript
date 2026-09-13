2df5118b11b9b032b68ae668b3d347255eacb341
shndlya-shiva <shandilyashivansh153@gmail.com>
2026-09-13 21:51:09 +0530

    updated file

--- 02_basics/Objects.js
+++ 02_basics/Objects.js
@@ -1,10 +1,15 @@
 // singleton object
 // object literals
 // Object.create() // creates a new object with the specified prototype object and properties.
+const mySym = Symbol("key1"); // Way to create a unique key for an object property
 const myObj = {
-    name: "Shiva",
+    name: "Shiva", "Full Name": "Shivansh Shandilya", // property with space in name
     age: 20,
-
+    isLoggedIn: false,
+    [mySym]: "key1" // computed property name
 }
-console.log(myObj.name); // Output: { name: 'Shiva'}
-console.log(myObj["name"]); // Output: { name: 'Shiva'}
+console.log(myObj.name); // Output: Shiva
+console.log(myObj["name"]); // Output: Shiva
+console.log(myObj[mySym]); // Output: key1, correct way to access the symbol property
+console.log(typeof myObj[mySym]); // Output: string
+console.log(myObj["Full Name"]); // Output: Shivansh Shandilya, correct way to access the property with space in name
