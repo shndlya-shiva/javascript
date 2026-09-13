76ddd2115dad2ce4d3e50de92aa448406df8daa8
shndlya-shiva <shandilyashivansh153@gmail.com>
2026-09-13 22:06:11 +0530

    updated file

--- 02_basics/Objects.js
+++ 02_basics/Objects.js
@@ -13,6 +13,21 @@ console.log(myObj["name"]); // Output: Shiva
 console.log(myObj[mySym]); // Output: key1, correct way to access the symbol property
 console.log(typeof myObj[mySym]); // Output: string
 console.log(myObj["Full Name"]); // Output: Shivansh Shandilya, correct way to access the property with space in name
+console.log("----------------------------------------------");
+// Adding a function to the object
+myObj.greet = function() {
+    console.log("Hello " + this.name); // this refers to the current object
+    console.log("Welcome to the world of JavaScript");
+}
+console.log(myObj.greet()); // correct way to add a new function to an object
+// Output: Hello Shiva, Welcome to the world of JavaScript, the function is called using the object reference
+// Another way to add a function to the object is using the object literal syntax
+myObj.greetTwo = function() {
+    console.log(`Hello ${this["Full Name"]}`); // using template literals also know as string interpolation
+    console.log(`This is a new function added to the object`);
+}
+console.log(myObj.greetTwo()); // correct way to add a new function to an object
+console.log("----------------------------------------------");
 myObj.age = 21; // updating the value of age property
 console.log(myObj.age);
 Object.freeze(myObj); // prevents modification of existing properties and values
